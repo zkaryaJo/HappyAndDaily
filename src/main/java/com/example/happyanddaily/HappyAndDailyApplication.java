e456b6354d034a5877bc2afdac72b91cf8237c2e
[zkaryajo] <46834158+zkaryaJo@users.noreply.github.com>
2023-05-05 14:49:22 +0900

    Initial commit

---
+++ src/main/java/com/example/happyanddaily/HappyAndDailyApplication.java
@@ -0,0 +1,13 @@
+package com.example.happyanddaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyAndDailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyAndDailyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/happyanddaily/HappyAndDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happyanddaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyAndDailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
