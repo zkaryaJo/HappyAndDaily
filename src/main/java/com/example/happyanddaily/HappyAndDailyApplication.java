cc79df4b165dbf24b59a0058828fd64d77797639
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2023-05-06 00:09:27 +0900

    temporary save

--- src/main/java/com/example/happyanddaily/HappyAndDailyApplication.java
+++ src/main/java/com/example/happyanddaily/HappyAndDailyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HappyAndDailyApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(HappyAndDailyApplication.class, args);
     }
 
