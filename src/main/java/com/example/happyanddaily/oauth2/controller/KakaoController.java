ef3bf493da62f1452c0e2d7f7a017209d83a9724
[zkaryajo] <88031716+kms310272@users.noreply.github.com>
2023-05-15 16:01:22 +0900

    gson 디펜던시 추가 필요없는 로그인 파일 삭제

--- src/main/java/com/example/happyanddaily/oauth2/controller/KakaoController.java
+++ src/main/java/com/example/happyanddaily/oauth2/controller/KakaoController.java
@@ -1,7 +1,11 @@
 package com.example.happyanddaily.oauth2.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/oauth")
 public class KakaoController {
 }
